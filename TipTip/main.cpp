80f61605db517677092ef4911bc024028a2114dc
wangxd <wang_sir_00@163.com>
2023-11-16 18:26:41 +0800

    support high dpi

--- TipTip/main.cpp
+++ TipTip/main.cpp
@@ -13,10 +13,17 @@ int main(int argc, char *argv[])
     HDC hdc = CreateDC(L"display", NULL, NULL, NULL);
     int ndpi = GetDeviceCaps(hdc, LOGPIXELSY);
     qputenv("QT_SCALE_FACTOR", QString::number(ndpi / 96.0).toUtf8());
+#endif
+
+#if (QT_VERSION < QT_VERSION_CHECK(6, 0, 0))
     QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 #endif
-   // QApplication::setQuitOnLastWindowClosed(false);
     QApplication a(argc, argv);
+
+#if (QT_VERSION < QT_VERSION_CHECK(6, 0, 0))
+    a.setAttribute(Qt::AA_UseHighDpiPixmaps);//背景图支持高清屏
+#endif
+
     MainWindow w;
     w.show();
 
